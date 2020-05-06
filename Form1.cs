d8b9625a3d9a4072728427e8a40ba612c9ea29f4
Gershon Naidoo <Nukphonik@gmail.com>
2020-05-06 05:28:22 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("We're really sorry", "Will you forgive us ?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) ==  DialogResult.Yes)
+                {
+                }
+        }
+    }
+}
